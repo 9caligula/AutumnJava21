54c7ee173504c2d988f47a7587df71d32d480c6b
Your Name <you@example.com>
2021-10-22 18:48:53 +0400

    commit

---
+++ Circle_Task/src/com/netcracker/point/struct/Circle.java
@@ -0,0 +1,9 @@
+package com.netcracker.point.struct;
+
+public class Circle {
+    private Point point;
+
+    public Circle(int x, int y) {
+        this.point = new Point(x, y);
+    }
+}
---
+++ Circle_Task/src/com/netcracker/point/struct/Point.java
@@ -0,0 +1,10 @@
+package com.netcracker.point.struct;
+
+class Point {
+    private int x, y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+}
